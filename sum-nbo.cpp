2f9622b6c650788f42be69df16db7c815de7d00b
mjbin <168641238+MJ-bin@users.noreply.github.com>
2026-07-24 03:10:31 +0900

    fix: Improve error handling and code readability
    Refactor error handling for file operations and improve readability.

--- sum-nbo.cpp
+++ sum-nbo.cpp
@@ -4,22 +4,33 @@
 #include <arpa/inet.h>
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
+    uint32_t sum = 0;
+	uint32_t network_byte_order;
+	
+	if (argc < 2) {
         fprintf(stderr, "Usage: %s <file1> [file2 ...]\n", argv[0]);
         return 1;
     }
-
-    uint32_t sum = 0;
-
-    // argv[0]은 프로그램 이름이므로 argv[1]부터 시작
+    
+	// argv[0]은 프로그램 이름이므로 argv[1]부터 시작
     for (int i = 1; i < argc; i++) {
         FILE* fp = fopen(argv[i], "rb");
-	uint32_t network_byte_order;
 
-        if (fp == NULL || fread(&network_byte_order, sizeof(network_byte_order), 1, fp) != 1) {
-            perror(argv[i]);
-            return 1;
-        }
+		// 파일 핸들 반환 실패
+		if (fp == NULL) {
+		    perror(argv[i]);
+		    return 1;
+		}
+
+		// 파일 읽기 실패
+		if (fread(&network_byte_order,
+		          sizeof(network_byte_order),
+		          1,
+		          fp) != 1) {
+		    		fprintf(stderr, "%s: failed to read 4 bytes\n", argv[i]);
+		    		fclose(fp);
+		    		return 1;
+		}
 
         fclose(fp);
 
